cf5153ac649acc53e45e79faff1ab3824e449e39
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-07-24 18:58:17 +0300

    fix method "post" change with const to let

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use((err, req, res, next) => {
     res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
 });
 
-app.post('v1/signup', (req, res) => {
+app.post('/v1/signup', (req, res) => {
 
-    const { username, fullname, country, gender, description, approvedTermsAndConditions } = req.body
-
+    const { username, fullname, country, description } = req.body;
+    let { gender, approvedTermsAndConditions } = req.body;
     if (!username && !fullname && !country || (!username || !fullname || !country)) {
-        res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.ERR_REQUIRED_FIELDS });
+        res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
     }
-    if (!/^\+?(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$/.test(username)) {
+    if (!/^\+?(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$/.test(username)) {//TODO:looc formate  ISO E.164
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (!/^([a-zA-Z\s\'\-]){3,64}$/.test(fullname)) {
@@ -40,47 +40,53 @@ app.post('v1/signup', (req, res) => {
 });
 
 
-app.post('v1/signin/:username', (req, res) => {
-    const { username, password, sendNotification } = req.params;
+app.post('/v1/signin/:username', (req, res) => {
+    const { username, password } = req.params;
+    let { sendNotification } = req.params;
     if (!username) {
-        return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
+        return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
     }
-    if (!password){
+    if (!password) {
         return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
     }
-    if (!/^\+?(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$/.test(username)){
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY }); 
+    if (!/^\+?(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$/.test(username)) {
+        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (!/^([a-zA-Z0-9\+\-\_]){10,64}/.test(password)) {
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
-    if (typeof sendNotification !== "boolean"){
+    if (typeof sendNotification !== "boolean") {
         sendNotification = false;
     }
-    return res.status(statusCodes.HTTP_STATUS_OK).json({ id:uuidv4() });
+    return res.status(statusCodes.HTTP_STATUS_OK).json({ id: uuidv4() });
 });
-app.put('v1/user/:id', (req, res) => {
+
+
+app.put('/v1/user/:id', (req, res) => {
     const { id, fullname, gender } = req.params;
 
-if (!id){
-    return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
-}
-if (!/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/.test(id))
-    return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
-if (!fullname && !gender){
-    return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
-}
-if (!/^([a-zA-Z\s\'\-]){3,64}$/.test(fullname)){
-    return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
-}
-if (fullname !== VALUE.FULLNAME_VALUE){
-    return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)   
-}
-return res.status(statusCodes.HTTP_STATUS_OK).json({ fullname });
+    if (!id) {
+        return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
+    }
+    if (!/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/.test(id)) {
+        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+    }
+    if (!fullname && !gender) {
+        return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
+    }
+    if (!/^([a-zA-Z\s\'\-]){3,64}$/.test(fullname)) {
+        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+    }
+    if (fullname !== VALUE.FULLNAME_VALUE) {
+        return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
+    }
+    return res.status(statusCodes.HTTP_STATUS_OK).json({ fullname });
 });
-app.get('v1/user', (req, res) => {
+
+
+app.get('/v1/user', (req, res) => {
     const { username, fullname, country, gender } = req.query;
-    if (!country){
+    if (!country) {
         return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
     }
     if (!/^([a-zA-Z]){2}$/.test(country) || !COUNTRYES.includes(country)) {
@@ -89,14 +95,14 @@ app.get('v1/user', (req, res) => {
     if (!/^([a-zA-Z\s\'\-]){3,64}$/.test(fullname)) {
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
-    if (country !== VALUE.COUNTRY_VALUE && fullname !== VALUE.FULLNAME_VALUE){
-        return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)   
+    if (country !== VALUE.COUNTRY_VALUE && fullname !== VALUE.FULLNAME_VALUE) {
+        return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
     }
-    return res.status(statusCodes.HTTP_STATUS_OK).json({ id:uuidv4(), fullname:VALUE.FULLNAME_VALUE });
+    return res.status(statusCodes.HTTP_STATUS_OK).json({ id: uuidv4(), fullname: VALUE.FULLNAME_VALUE });
 });
-   
 
-    app.listen(PORT, () => {
-        console.log(`server runs on the port ${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`server runs on the port ${PORT}`);
 
 })
