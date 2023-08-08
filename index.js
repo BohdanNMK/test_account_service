fdb171893b012f56552542591baab1db0b21d458
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-08-08 21:46:53 +0300

    transferring them from 'regularExp' in 'variables'

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
-const statusCodes = require('http2').constants
-const { COUNTRIES, GENDER, VALUE, PORT, HTTP_MESSAGES } = require('./variable')
-const { v4: uuidv4 } = require('uuid')
-const { USER_NAME_REG_EXP, FULL_NAME_REG_EXP, COUNTRY_REG_EXP, GENDER_REG_EXP,
-    DESCRIRTION_REG_EXP, PASSWORD_REG_EXP, UUID_REG_EXP } = require('./regularExpression')
+const {HTTP_STATUS_BAD_REQUEST,HTTP_STATUS_INTERNAL_SERVER_ERROR,HTTP_STATUS_UNPROCESSABLE_ENTITY,
+       HTTP_STATUS_CREATED,HTTP_STATUS_OK,HTTP_STATUS_NO_CONTENT} = require('http2').constants
+const {STATIC_DATA:{METHOD:{POST}, VALUE:{FULLNAME_VALUE,COUNTRY_VALUE, OTHER_VALUE,BOOLEAN_VALUE},
+       PORT, COUNTRIES, GENDER,  HTTP_MESSAGES:{ERR4XX:{BAD_REQUEST},UNPROCESSABLE_ENTITY,INTERNAL_SERVER_ERROR}}, 
+       REGEXP: { USER_NAME_REG_EXP, FULL_NAME_REG_EXP, COUNTRY_REG_EXP, GENDER_REG_EXP,
+       DESCRIRTION_REG_EXP, PASSWORD_REG_EXP, UUID_REG_EXP } } = require('./variable')
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(express.json());
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
+    res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: INTERNAL_SERVER_ERROR });
 });
+
+
 app.use((req, res, next) => {
     const contentType = req.header('content-type');
     const contentLength = req.header('content-length');
@@ -22,7 +27,7 @@ app.use((req, res, next) => {
   });
 
   app.use('/v1/signup', (req, res, next) => {
-     if (req.method !== 'POST'){
+     if (req.method !== POST){
         
         next();
     }
@@ -30,35 +35,35 @@ app.use((req, res, next) => {
     let { gender, approvedTermsAndConditions } = req.body;
 
     if (!username && !fullname && !country || (!username || !fullname || !country)) {
-        return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
+        return res.status(HTTP_STATUS_BAD_REQUEST).json({ error: BAD_REQUEST });
     }
     if (!USER_NAME_REG_EXP.test(username)) {
         
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!FULL_NAME_REG_EXP.test(fullname)) {
        
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!COUNTRY_REG_EXP.test(country) || !COUNTRIES.includes(country.toUpperCase())) {
        
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!GENDER_REG_EXP.test(gender) || !GENDER.includes(gender.toLowerCase())) {
-        gender = 'other';
+        gender = OTHER_VALUE;
     }
     if (description && !DESCRIRTION_REG_EXP.test(description)) {
        
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
-    if (typeof approvedTermsAndConditions !== "boolean") {
+    if (typeof approvedTermsAndConditions !== BOOLEAN_VALUE) {
+
         approvedTermsAndConditions = false;
     }
 
-    return res.status(statusCodes.HTTP_STATUS_CREATED).json({ id: uuidv4() });
+    return res.status(HTTP_STATUS_CREATED).json({ id: uuidv4() });
 
 });
-
 
 
 app.post('/v1/signin/:username', (req, res) => {
@@ -67,21 +72,21 @@ app.post('/v1/signin/:username', (req, res) => {
     let { sendNotification } = req.body;
 
     if (!username) {
-        return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
+        return res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: INTERNAL_SERVER_ERROR });
     }
     if (!password) {
-        return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
+        return res.status(HTTP_STATUS_BAD_REQUEST).json({ error: BAD_REQUEST });
     }
     if (!USER_NAME_REG_EXP.test(username)) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!PASSWORD_REG_EXP.test(password)) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
-    if (typeof sendNotification !== "boolean") {
+    if (typeof sendNotification !== BOOLEAN_VALUE) {
         sendNotification = false;
     }
-    return res.status(statusCodes.HTTP_STATUS_OK).json({ id: uuidv4() });
+    return res.status(HTTP_STATUS_OK).json({ id: uuidv4() });
 });
 
 
@@ -90,43 +95,48 @@ app.put('/v1/user/:id', (req, res) => {
     const { fullname, gender } = req.body;
 
     if (!id) {
-        return res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
+        return res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: INTERNAL_SERVER_ERROR });
     }
     if (!UUID_REG_EXP.test(id)) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!fullname && !gender) {
-        return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
+        return res.sendStatus(HTTP_STATUS_NO_CONTENT)
     }
     if (!FULL_NAME_REG_EXP.test(fullname)) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
-    if (fullname === VALUE.FULLNAME_VALUE) {
-        return res.status(statusCodes.HTTP_STATUS_OK).json({ fullname });
+    if (fullname === FULLNAME_VALUE) {
+        return res.status(HTTP_STATUS_OK).json({ fullname });
     }
 
-    return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
+    return res.sendStatus(HTTP_STATUS_NO_CONTENT)
 });
 
 
 app.get('/v1/user', (req, res) => {
     const { username, fullname, country, gender } = req.query;
     if (!country) {
-        return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
+        return res.status(HTTP_STATUS_BAD_REQUEST).json({ error: BAD_REQUEST });
     }
     if (!COUNTRY_REG_EXP.test(country) && !COUNTRIES.includes(country.toUpperCase())) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
     if (!FULL_NAME_REG_EXP.test(fullname)) {
-        return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
+        return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: UNPROCESSABLE_ENTITY });
     }
-    if (country.toLocaleUpperCase() === VALUE.COUNTRY_VALUE && fullname === VALUE.FULLNAME_VALUE) {
-        return res.status(statusCodes.HTTP_STATUS_OK).json({ id: uuidv4(), fullname: VALUE.FULLNAME_VALUE });
+    if (country.toLocaleUpperCase() === COUNTRY_VALUE && fullname === FULLNAME_VALUE) {
+        return res.status(HTTP_STATUS_OK).json({ id: uuidv4(), fullname: FULLNAME_VALUE });
     }
 
-    return res.sendStatus(statusCodes.HTTP_STATUS_NO_CONTENT)
+    return res.sendStatus(HTTP_STATUS_NO_CONTENT)
 });
 
+app.use((err, req, res, next) => {
+  
+    console.error(error.stack);
+    res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: INTERNAL_SERVER_ERROR });
+});
 
 app.listen(PORT, () => {
     console.log(`server runs on the port ${PORT}`)
--- variable.js
+++ variable.js
@@ -1,28 +1,50 @@
-module.exports = {
+const STATIC_DATA = {
   PORT: 33112,
   COUNTRIES: ['AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ', 'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CW', 'CX', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ', 'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FK', 'FM', 'FO', 'FR', 'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY', 'HK', 'HM', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ', 'OM', 'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY', 'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SX', 'SY', 'SZ', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ', 'UA', 'UG', 'UM', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU', 'WF', 'WS', 'YE', 'YT', 'ZA', 'ZM', 'ZW'],
   GENDER: ['male', 'female', 'other'],
   VALUE: {
     FULLNAME_VALUE: "Cartman",
     COUNTRY_VALUE: "US",
+    OTHER_VALUE: 'other',
+    BOOLEAN_VALUE: "boolean"
+
+  },
+  METHOD:{
+    POST: 'POST'
   },
   HTTP_MESSAGES: {
-    'ERR400': {
+    'ERR4XX': {
       'BAD_REQUEST': "Bad Request",
       'ERR_REQUIRED_FIELDS': "'name' and 'type' are required fields",
       'ERR_REQUIRED_FIELDS_NAME': "'name' are required fields",
       'MAX_LENGH': "'name' field must be a string with a maximum of 255 characters"
     },
+    'ERR2XX':{
     'OK': "OK",
-    'VARIABLE_UNDEFINED': 'variable is undefined',
     'CREATED': "Created",
     'NO_CONTENT': "No Content",
+  },
+    
+    'VARIABLE_UNDEFINED': 'variable is undefined',
     'MUST_BE_NUMBER': "Field 'type' must be a number",
     'NOT_FOUND': "Not Found",
     'UNPROCESSABLE_ENTITY': "Unprocessable Entity",
     'INTERNAL_SERVER_ERROR': "Internal Server Error"
   },
-
 };
-//const COUNTRY_REG_EXP = new RegExp(/^(COUNTRIES.join("|"))$, "i");
+const REGEXP = {
+  USER_NAME_REG_EXP: new RegExp ('^\\+?([1-9]){6,15}$'),
+  FULL_NAME_REG_EXP: new RegExp(/^([a-zA-Z\s\'\-]){3,64}$/),
+  COUNTRY_REG_EXP: new RegExp(STATIC_DATA.COUNTRIES.join('|'), 'i'),
+  GENDER_REG_EXP: new RegExp(STATIC_DATA.GENDER.join('|'), 'i'),
+  DESCRIRTION_REG_EXP: new RegExp(/^([a-zA-Z0-9\s\,\.\!\-\'\_]){1,2000}$/),
+  PASSWORD_REG_EXP: new RegExp(/^([a-zA-Z0-9\+\-\_]){10,64}$/),
+  UUID_REG_EXP: new RegExp(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/)
+};
+
+module.exports = {
+  STATIC_DATA,
+  REGEXP
+};
+
 
