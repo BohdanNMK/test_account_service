5fbbbd6d490e0bd4655ec582178d93bf239d970c
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-08-08 12:53:54 +0300

    change method 'post' for method 'use'

--- index.js
+++ index.js
@@ -9,39 +9,56 @@ const { USER_NAME_REG_EXP, FULL_NAME_REG_EXP, COUNTRY_REG_EXP, GENDER_REG_EXP,
 app.use(express.json());
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
+    res.status(statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ error: HTTP_MESSAGES.INTERNAL_SERVER_ERROR });
 });
+app.use((req, res, next) => {
+    const contentType = req.header('content-type');
+    const contentLength = req.header('content-length');
+  
+    console.log('content-type:', contentType);
+    console.log('content-length:', contentLength);
+  
+    next();
+  });
 
-
-
-app.post('/v1/signup', (req, res) => {
-
-
+  app.use('/v1/signup', (req, res, next) => {
+     if (req.method !== 'POST'){
+        
+        next();
+    }
     const { username, fullname, country, description } = req.body;
     let { gender, approvedTermsAndConditions } = req.body;
+
     if (!username && !fullname && !country || (!username || !fullname || !country)) {
-        res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
+        return res.status(statusCodes.HTTP_STATUS_BAD_REQUEST).json({ error: HTTP_MESSAGES.ERR400.BAD_REQUEST });
     }
     if (!USER_NAME_REG_EXP.test(username)) {
+        
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (!FULL_NAME_REG_EXP.test(fullname)) {
+       
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (!COUNTRY_REG_EXP.test(country) || !COUNTRIES.includes(country.toUpperCase())) {
+       
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (!GENDER_REG_EXP.test(gender) || !GENDER.includes(gender.toLowerCase())) {
         gender = 'other';
     }
     if (description && !DESCRIRTION_REG_EXP.test(description)) {
+       
         return res.status(statusCodes.HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ error: HTTP_MESSAGES.UNPROCESSABLE_ENTITY });
     }
     if (typeof approvedTermsAndConditions !== "boolean") {
-        approvedTermsAndConditions = false
+        approvedTermsAndConditions = false;
     }
+
     return res.status(statusCodes.HTTP_STATUS_CREATED).json({ id: uuidv4() });
+
 });
+
 
 
 app.post('/v1/signin/:username', (req, res) => {
@@ -112,6 +129,5 @@ app.get('/v1/user', (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`server runs on the port ${PORT}`);
-
+    console.log(`server runs on the port ${PORT}`)
 })
